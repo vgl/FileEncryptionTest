9818ec6925cf5d6ec317fea59aa97df4ea059e59
vgolubev <slack.master99@gmail.com>
2014-08-25 11:44:33 +0400

    initial commit
    initial commit

---
+++ FileEncryption/PgpTest/Controllers/HomeController.cs
@@ -0,0 +1,36 @@
+﻿using System.IO;
+using System.Web.Mvc;
+
+using FileEncryption.WebApp;
+
+namespace PgpTest.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            var pathInput = Server.MapPath("~/App_Data/Image.png");
+            var pathOutput = Server.MapPath("~/App_Data/data.png");
+            var pathDecrypted = Server.MapPath("~/App_Data/after.png");
+            var key = "123456789098765432125432124654567";
+            FileEncryptionHelper.Encrypt(pathInput, pathOutput, key);
+            FileEncryptionHelper.Decrypt(pathOutput, pathDecrypted, key);
+
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ FileEncryption/PgpTest/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="FileEncryption.WebApp.MvcApplication" Language="C#" %>
