69a0798e3845a563afeb585e0b570b12e528c72b
vgolubev <slack.master99@gmail.com>
2014-08-26 08:58:14 +0400

    refactoring

---
+++ FileEncryption/FileEncryption/Controllers/HomeController.cs
@@ -0,0 +1,40 @@
+﻿using System.IO;
+using System.Web.Mvc;
+
+namespace FileEncryption.WebApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            var pathInput = Server.MapPath("~/App_Data/Image.png");
+            var pathOutput = Server.MapPath("~/App_Data/data.png");
+            var pathDecrypted = Server.MapPath("~/App_Data/after.png");
+            var key = "123456789098765432125432124654567";
+
+            using (var stream = new FileStream(pathInput, FileMode.Open))
+            {
+                FileEncryptionHelper.Encrypt(stream, pathOutput, key);
+            }
+
+            //FileEncryptionHelper.Encrypt(pathInput, pathOutput, key);
+            FileEncryptionHelper.Decrypt(pathOutput, pathDecrypted, key);
+
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ FileEncryption/FileEncryption/Startup.cs
@@ -0,0 +1,17 @@
+﻿using FileEncryption.WebApp;
+
+using Microsoft.Owin;
+
+using Owin;
+
+//[assembly: OwinStartup(typeof(Startup))]
+namespace FileEncryption.WebApp
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            //ConfigureAuth(app);
+        }
+    }
+}
